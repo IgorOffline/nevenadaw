9a4f949fa4b81dde42fb560aaa81b062ef9eae6f
Igor Durbek <igor@igordurbek.com>
2025-10-20 07:25:33 +0200

    +12 rectangles

--- __practice__/bevyfour/src/main.rs
+++ __practice__/bevyfour/src/main.rs
@@ -6,32 +6,36 @@ fn main() {
     App::new()
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
-                resolution: WindowResolution::new(800, 600),
+                resolution: WindowResolution::new(1280, 720),
                 title: "Bevyrectangle".to_string(),
                 ..default()
             }),
             ..default()
         }))
         .add_systems(Startup, setup)
-        .add_systems(Update, (move_rectangle, despawn_and_respawn_rectangle))
+        //.add_systems(Update, (move_rectangle, despawn_and_respawn_rectangle))
         .run();
 }
 
 fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
     commands.spawn(Camera2d::default());
 
-    spawn_rectangle(&mut commands, &asset_server);
+    spawn_rectangles(&mut commands, &asset_server);
 }
 
-fn spawn_rectangle(commands: &mut Commands, asset_server: &Res<AssetServer>) {
-    commands.spawn((
-        Sprite::from_image(asset_server.load("rectangle_03A9F4_100w100h.png")),
-        Transform::from_xyz(0.0, 0.0, 0.0),
-        Movable {
-            direction: random::<bool>(),
-            speed: 125.0,
-        },
-    ));
+fn spawn_rectangles(commands: &mut Commands, asset_server: &Res<AssetServer>) {
+    for i in 0..12 {
+        let mut transform: Transform = Transform::from_xyz(-225.0 + 65.0 * i as f32, 0.0, 0.0);
+        transform = transform.with_scale(Vec3::new(0.5, 0.5, 1.0));
+        commands.spawn((
+            Sprite::from_image(asset_server.load("rectangle_03A9F4_100w100h.png")),
+            Transform::from(transform),
+            Movable {
+                direction: random::<bool>(),
+                speed: 0.0,
+            },
+        ));
+    }
 }
 
 #[derive(Component)]
@@ -40,7 +44,7 @@ struct Movable {
     speed: f32,
 }
 
-fn move_rectangle(time: Res<Time>, mut query: Query<(&mut Transform, &Movable)>) {
+fn _move_rectangle(time: Res<Time>, mut query: Query<(&mut Transform, &Movable)>) {
     for (mut transform, mover) in query.iter_mut() {
         let mut add_to_x = mover.speed * time.delta().as_secs_f32();
         if !mover.direction {
@@ -49,7 +53,7 @@ fn move_rectangle(time: Res<Time>, mut query: Query<(&mut Transform, &Movable)>)
         transform.translation.x += add_to_x
     }
 }
-fn despawn_and_respawn_rectangle(
+fn _despawn_and_respawn_rectangle(
     mut commands: Commands,
     asset_server: Res<AssetServer>,
     query: Query<(Entity, &Transform), With<Movable>>,
@@ -58,7 +62,7 @@ fn despawn_and_respawn_rectangle(
         if transform.translation.x < -125.0 || transform.translation.x > 125.0 {
             commands.entity(entity).despawn();
 
-            spawn_rectangle(&mut commands, &asset_server);
+            spawn_rectangles(&mut commands, &asset_server);
         }
     }
 }
