bd26fd03507ff4761dd2d9149ba7d38019eef04d
IgorOffline <73474119+IgorOffline@users.noreply.github.com>
2025-11-03 06:02:00 +0100

    PR12: Zig +log_info_args
    PR12: Zig +log_info_args

--- __practice__/ziginit/zigmulti/core/src/root.zig
+++ __practice__/ziginit/zigmulti/core/src/root.zig
@@ -1,6 +1,22 @@
 const std = @import("std");
 
-pub fn sum(a: i32, b: i32) i32 {
-    std.debug.print("Core: Calculating {d} + {d}\n", .{ a, b });
+pub fn math_sum(a: i32, b: i32) i32 {
     return a + b;
+}
+
+pub fn log_info(message: ?[]const u8) void {
+    const write: []const u8 = message orelse "";
+    std.debug.print("{s}\n", .{write});
+}
+
+pub fn log_info_args(comptime format: []const u8, args: anytype) void {
+    var buffer: [512]u8 = undefined;
+    const message = std.fmt.bufPrint(&buffer, format, args) catch format;
+
+    std.fs.File.stdout().writeAll(message) catch |err| {
+        std.debug.print("err-9ff96916: {s}\n", .{@errorName(err)});
+    };
+    std.fs.File.stdout().writeAll("\n") catch |err| {
+        std.debug.print("err-d03e7948: {s}\n", .{@errorName(err)});
+    };
 }
--- __practice__/ziginit/zigmulti/frontend/src/main.zig
+++ __practice__/ziginit/zigmulti/frontend/src/main.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const core = @import("core");
 
 pub fn main() !void {
-    std.debug.print("<ZIG-MULTI-FRONTEND-MAIN>\n", .{});
-    const sum = core.sum(2, 3);
-    std.debug.print("Sum: {d}\n", .{sum});
+    core.log_info("<ZIG-MULTI-FRONTEND-MAIN>");
+    const sum = core.math_sum(2, 3);
+    core.log_info_args("Sum: {d}", .{sum});
 }
