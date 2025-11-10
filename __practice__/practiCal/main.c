9cfb1a0c33368265096d4fe201c0692da75ee059
Igor Durbek <igor@igordurbek.com>
2025-11-10 03:44:42 +0100

    main.c Clang 21.1.3

--- __practice__/practiCal/main.c
+++ __practice__/practiCal/main.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
 
+#ifndef __clang__
+#error "Please compile with Clang"
+#endif
+
 int main(void) {
-  printf("Hello, World!\n");
+  printf("Successfully compiled with Clang %s (%d.%d.%d)\n", __clang_version__, __clang_major__, __clang_minor__,
+         __clang_patchlevel__);
   return 0;
 }
