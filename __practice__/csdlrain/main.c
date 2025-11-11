e3ec5715ab3361548e214abfab001713958f35bd
Igor Durbek <igor@igordurbek.com>
2025-11-11 09:38:48 +0100

    +bgfx (crashing)

--- __practice__/csdlrain/main.c
+++ __practice__/csdlrain/main.c
@@ -1,57 +1,75 @@
 #include <SDL3/SDL.h>
 #include <SDL3_ttf/SDL_ttf.h>
+#include <bgfx/c99/bgfx.h>
 #include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "platform.h"
+
+#if BX_PLATFORM_WINDOWS == 0
+#error "Windows expected"
+#endif
 
 int main() {
   SDL_Init(SDL_INIT_VIDEO);
   TTF_Init();
 
-  SDL_Window *window = SDL_CreateWindow("SDL Rain", 1280, 720, 0);
-  SDL_Renderer *renderer = SDL_CreateRenderer(window, NULL);
-  const float font_size = 52;
+  const int window_width = 1280;
+  const int window_height = 720;
+  SDL_Window *window = SDL_CreateWindow("SDL/BGFX Rain", window_width, window_height, 0);
+
+  const float font_size = 52.F;
   TTF_Font *font_latin = TTF_OpenFont("C:\\igoroffline\\fonts\\IosevkaTerm-Regular.ttf", font_size);
   TTF_Font *font_hangul = TTF_OpenFont("C:\\igoroffline\\fonts\\NotoSansKR.ttf", font_size);
   TTF_AddFallbackFont(font_latin, font_hangul);
 
+  bgfx_init_t init;
+  bgfx_init_ctor(&init);
+  init.type = BGFX_RENDERER_TYPE_COUNT;
+
+  bgfx_platform_data_t pd = {0};
+#ifdef SDL_PLATFORM_WIN32
+  void *hwnd = SDL_GetPointerProperty(SDL_GetWindowProperties(window), SDL_PROP_WINDOW_WIN32_HWND_POINTER, NULL);
+  pd.nwh = hwnd;
+#else
+  pd.nwh = NULL;
+#endif
+  bgfx_set_platform_data(&pd);
+
+  init.resolution.width = (uint32_t) window_width;
+  init.resolution.height = (uint32_t) window_height;
+  init.resolution.reset = BGFX_RESET_VSYNC;
+
+  const uint32_t view_width_32 = window_width;
+  const uint32_t view_height_32 = window_height;
+
+  bgfx_set_view_clear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, 0xFF212121, 1.0f, 0);
+  bgfx_set_view_rect(0, 0, 0, (uint16_t) view_width_32, (uint16_t) view_height_32);
+
   SDL_Event e;
   bool quit = false;
   while (!quit) {
-    if (SDL_PollEvent(&e)) {
+    while (SDL_PollEvent(&e)) {
       switch (e.type) {
         case SDL_EVENT_QUIT:
           quit = true;
           break;
-        default: break ;
+        default:
+          break;
       }
     }
-    const SDL_Color light_green = {139, 195, 74, 255};
-    const SDL_Color primary_text_dark = {33, 33, 33, 255};
-    SDL_Surface *text_surf = TTF_RenderText_Solid(font_latin, "Hello반말Ipsum나LoremČćŠđŽRain", 0, light_green);
-    SDL_Texture *text = SDL_CreateTextureFromSurface(renderer, text_surf);
 
-    SDL_FRect dest = (SDL_FRect){
-      .x = 200,
-      .y = 200,
-      .w = (float) text_surf->w,
-      .h = (float) text_surf->h
-    };
-    SDL_RenderTexture(renderer, text, NULL, &dest);
-
-    SDL_DestroyTexture(text);
-    SDL_DestroySurface(text_surf);
-    SDL_RenderPresent(renderer);
-
-    SDL_SetRenderDrawColor(renderer, primary_text_dark.r, primary_text_dark.g, primary_text_dark.b,
-                           primary_text_dark.a);
-    SDL_RenderClear(renderer);
+    bgfx_touch(0);
+    bgfx_frame(false);
   }
 
-  TTF_CloseFont(font_latin);
-  TTF_CloseFont(font_hangul);
-  SDL_DestroyRenderer(renderer);
+  bgfx_shutdown();
+  if (font_latin) TTF_CloseFont(font_latin);
+  if (font_hangul) TTF_CloseFont(font_hangul);
   SDL_DestroyWindow(window);
   TTF_Quit();
   SDL_Quit();
 
-  return 0;
+  return EXIT_SUCCESS;
 }
