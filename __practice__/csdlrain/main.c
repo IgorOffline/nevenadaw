04381a139932f06d26576fcc0b666591f7310d6a
IgorOffline <73474119+IgorOffline@users.noreply.github.com>
2025-11-11 10:49:54 +0100

    PR30 SDL/BGFX
    PR30 SDL/BGFX

--- __practice__/csdlrain/main.c
+++ __practice__/csdlrain/main.c
@@ -1,144 +1,111 @@
-#ifdef _MSC_VER
-#define _CRTDBG_MAP_ALLOC
-#include <crtdbg.h>
-#endif // _MSC_VER
+#define SDL_MAIN_HANDLED
 
+#include <SDL3/SDL.h>
+#include <bgfx/c99/bgfx.h>
+#include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 
-#define SDL_MAIN_USE_CALLBACKS 1
-#include <SDL3/SDL.h>
-#include <SDL3/SDL_main.h>
+const int window_width = 1280;
+const int window_height = 720;
 
-typedef struct {
-  int32_t rain_context_number;
-} RainContext;
+bool init_window(SDL_Window *window) {
+    bgfx_init_t init;
+    bgfx_init_ctor(&init);
 
-typedef struct {
-  SDL_Window *window;
-  SDL_Renderer *renderer;
-  RainContext rain_context;
-  Uint64 last_step_ms;
-  bool button_message_pressed;
-  bool should_quit;
-} AppState;
+    bgfx_platform_data_t pd = {0};
 
-SDL_AppResult SDL_AppIterate(void *appstate) {
-  AppState *as = appstate;
-  if (!as) {
-    return SDL_APP_FAILURE;
-  }
-
-  if (as->button_message_pressed) {
-    printf("[F]\n");
-    fflush(stdout);
-    as->button_message_pressed = false;
-  }
-
-  if (as->should_quit) {
-    return SDL_APP_SUCCESS;
-  }
-
-  const SDL_Color grey_secondary_text = {.r = 117, .g = 117, .b = 117, .a = 255};
-  if (as->renderer) {
-    SDL_SetRenderDrawColor(as->renderer, grey_secondary_text.r, grey_secondary_text.g, grey_secondary_text.b,
-                           grey_secondary_text.a);
-    SDL_RenderClear(as->renderer);
-    SDL_RenderPresent(as->renderer);
-  }
-
-  return SDL_APP_CONTINUE;
-}
-
-SDL_AppResult SDL_AppInit(void **appstate, int argc, char *argv[]) {
-  printf("2\n");
-  (void) argc;
-  (void) argv;
-
-#ifdef __clang__
-  printf("Successfully compiled with Clang %s (%d.%d.%d)\n", __clang_version__, __clang_major__, __clang_minor__,
-         __clang_patchlevel__);
-#elif defined(_MSC_VER)
-  printf("Successfully compiled with MSVC version %d (%d.%d.%d.%d)\n",
-         _MSC_VER,
-         _MSC_VER / 100,
-         _MSC_VER % 100,
-         _MSC_FULL_VER % 100000,
-         _MSC_BUILD);
-#else
-  printf("No Clang or MSVC detected - using another compiler\n");
-#endif
-#ifdef _MSC_VER
-  _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
-  _CrtSetReportMode(_CRT_WARN, _CRTDBG_MODE_FILE);
-  _CrtSetReportFile(_CRT_WARN, _CRTDBG_FILE_STDERR);
-  _CrtSetReportMode(_CRT_ERROR, _CRTDBG_MODE_FILE);
-  _CrtSetReportFile(_CRT_ERROR, _CRTDBG_FILE_STDERR);
-  _CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_FILE);
-  _CrtSetReportFile(_CRT_ASSERT, _CRTDBG_FILE_STDERR);
+#ifdef SDL_PLATFORM_WIN32
+    void *hwnd = SDL_GetPointerProperty(SDL_GetWindowProperties(window), SDL_PROP_WINDOW_WIN32_HWND_POINTER, NULL);
+    pd.nwh = hwnd;
 #endif
 
-  AppState *as = SDL_calloc(1, sizeof(AppState));
-  if (!as) {
-    SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Failed to allocate AppState\n");
-    return SDL_APP_FAILURE;
-  }
-  as->last_step_ms = SDL_GetTicks();
+    pd.ndt = NULL;
+    pd.context = NULL;
+    pd.backBuffer = NULL;
+    pd.backBufferDS = NULL;
 
-  as->window = SDL_CreateWindow("SDL Rain", 1280, 720, 0);
-  if (!as->window) {
-    SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Failed to create window: %s\n", SDL_GetError());
-    SDL_free(as);
-    return SDL_APP_FAILURE;
-  }
+    bgfx_set_platform_data(&pd);
 
-  as->renderer = SDL_CreateRenderer(as->window, NULL);
-  if (!as->renderer) {
-    SDL_LogError(SDL_LOG_CATEGORY_ERROR, "Failed to create renderer: %s\n", SDL_GetError());
-    SDL_DestroyWindow(as->window);
-    SDL_free(as);
-    return SDL_APP_FAILURE;
-  }
+    init.resolution.width = (uint32_t) window_width;
+    init.resolution.height = (uint32_t) window_height;
+    init.resolution.reset = BGFX_RESET_VSYNC;
+    init.platformData = pd;
 
-  *appstate = as;
-  return SDL_APP_CONTINUE;
-}
+    const bgfx_renderer_type_t renderers[] = {
+        BGFX_RENDERER_TYPE_DIRECT3D12,
+        BGFX_RENDERER_TYPE_DIRECT3D11,
+        BGFX_RENDERER_TYPE_VULKAN,
+        BGFX_RENDERER_TYPE_OPENGL,
+    };
 
-SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) {
-  AppState *as = appstate;
+    for (int i = 0; i < sizeof(renderers) / sizeof(renderers[0]); i++) {
+        init.type = renderers[i];
+        if (bgfx_init(&init)) {
+            bgfx_set_view_rect(0, 0, 0, (uint16_t) window_width, (uint16_t) window_height);
 
-  if (event) {
-    switch (event->type) {
-      case SDL_EVENT_QUIT:
-      case SDL_EVENT_WINDOW_CLOSE_REQUESTED:
-        if (as) as->should_quit = true;
-        return SDL_APP_CONTINUE;
-      case SDL_EVENT_KEY_DOWN:
-        if (event->key.scancode == SDL_SCANCODE_F) {
-          as->button_message_pressed = true;
-          return SDL_APP_CONTINUE;
+            return true;
         }
-        if (event->key.scancode == SDL_SCANCODE_ESCAPE) {
-          as->should_quit = true;
-          return SDL_APP_CONTINUE;
-        }
-        return SDL_APP_CONTINUE;
-      default:
-        return SDL_APP_CONTINUE;
     }
-  }
 
-  return SDL_APP_CONTINUE;
+    fprintf(stderr, "ERROR: Failed to initialize BGFX with any renderer!\n");
+
+    return false;
 }
 
-void SDL_AppQuit(void *appstate, SDL_AppResult result) {
-  printf("4\n");
+int main(int argc, char *argv[]) {
+    (void) argc;
+    (void) argv;
 
-  (void) result;
-  if (appstate != NULL) {
-    AppState *as = appstate;
-    SDL_DestroyRenderer(as->renderer);
-    SDL_DestroyWindow(as->window);
-    SDL_free(as);
-  }
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
+        fprintf(stderr, "SDL initialization failed: %s\n", SDL_GetError());
+        return EXIT_FAILURE;
+    }
+
+    SDL_Window *window = SDL_CreateWindow(
+        "SDL/BGFX Rain",
+        window_width,
+        window_height,
+        SDL_WINDOW_HIDDEN | SDL_WINDOW_RESIZABLE
+    );
+
+    if (window == NULL) {
+        fprintf(stderr, "Window creation failed: %s\n", SDL_GetError());
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+
+    if (!init_window(window)) {
+        SDL_DestroyWindow(window);
+        SDL_Quit();
+        return EXIT_FAILURE;
+    }
+
+    SDL_ShowWindow(window);
+    bool quit = false;
+    SDL_Event event;
+
+    printf("Renderer backend: %s\n", bgfx_get_renderer_name(bgfx_get_renderer_type()));
+
+    while (!quit) {
+        while (SDL_PollEvent(&event)) {
+            if (event.type == SDL_EVENT_QUIT) {
+                quit = true;
+            }
+        }
+
+        bgfx_touch(0);
+        const SDL_Color primary_text_dark_raw = {33, 33, 33, 255};
+        const uint32_t primary_text_dark = primary_text_dark_raw.r << 24 | primary_text_dark_raw.g << 16 |
+                                           primary_text_dark_raw.b << 8 | primary_text_dark_raw.a;
+        bgfx_set_view_clear(0, BGFX_CLEAR_COLOR | BGFX_CLEAR_DEPTH, primary_text_dark, 1.0f, 0);
+        bgfx_frame(false);
+    }
+
+    bgfx_shutdown();
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
+    return EXIT_SUCCESS;
 }
