5b56eb04356b6e346b809ea3d48f600c9f304d7c
Igor Durbek <igor@igordurbek.com>
2025-11-14 12:55:21 +0100

    +[glew-cmake glfw glm] pt. 2

--- __practice__/otokoislandia/src/main.cpp
+++ __practice__/otokoislandia/src/main.cpp
@@ -1,5 +1,9 @@
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 #include <toml++/toml.hpp>
+
+int graphics();
 
 int main() {
   std::cout << "<START>" << std::endl;
@@ -14,7 +18,32 @@ int main() {
     return EXIT_FAILURE;
   }
 
-  std::cout << "<END>" << std::endl;
+  const auto graphics_status = graphics();
+
+  std::cout << "<END>" << graphics_status << std::endl;
+
+  return EXIT_SUCCESS;
+}
+
+int graphics() {
+  if (!glfwInit()) return EXIT_FAILURE;
+
+  GLFWwindow* window = glfwCreateWindow(1280, 720, "otokoislandia", NULL, NULL);
+
+  if (!window) {
+    glfwTerminate();
+    return EXIT_FAILURE;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  while (!glfwWindowShouldClose(window)) {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
 
   return EXIT_SUCCESS;
 }
