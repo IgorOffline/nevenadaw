7cb48383ab9f5cfc4804ccd02e8610c24785e440
Igor Durbek <igor@igordurbek.com>
2025-11-14 07:18:12 +0100

    +third party includes

---
+++ __practice__/danas/common/font/font_manager.cpp
@@ -0,0 +1,22 @@
+#include <bgfx/bgfx.h>
+#include <bx/bx.h>
+// LATER #include <stb/stb_truetype.h>
+
+#include "../common.h"
+
+BX_PRAGMA_DIAGNOSTIC_PUSH()
+BX_PRAGMA_DIAGNOSTIC_IGNORED_MSVC(
+    4244)  //  warning C4244: '=': conversion from 'double' to 'float', possible
+           //  loss of data
+BX_PRAGMA_DIAGNOSTIC_IGNORED_MSVC(
+    4701)  //  warning C4701: potentially uninitialized local variable 'pt' used
+#define SDF_IMPLEMENTATION
+// LATER #include <sdf/sdf.h>
+BX_PRAGMA_DIAGNOSTIC_POP()
+
+#include <wchar.h>  // wcslen
+
+#include "../cube_atlas.h"
+#include "allocator.h"
+#include "font_manager.hpp"
+#include "unordered_map.h"
