3000840f23324ef2589e82cc37fba4ed7fb4d1aa
Igor Durbek <igor@igordurbek.com>
2025-11-14 12:14:46 +0100

    +main.toml

--- __practice__/otokoislandia/src/main.cpp
+++ __practice__/otokoislandia/src/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
+#include <toml++/toml.hpp>
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  std::cout << "<START>" << std::endl;
 
-  return 0;
+  try {
+    const toml::table tbl = toml::parse_file(
+        R"(C:\igoroffline\nevenadaw\__practice__\otokoislandia\config\main.toml)");
+    std::cout << tbl << std::endl;
+  } catch (const toml::parse_error& err) {
+    std::cerr << "Parsing failed:" << std::endl << err << std::endl;
+
+    return EXIT_FAILURE;
+  }
+
+  std::cout << "<END>" << std::endl;
+
+  return EXIT_SUCCESS;
 }
