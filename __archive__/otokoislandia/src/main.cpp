e17a660c2f30d1f011479889e5240ffe7b212f42
Igor Durbek <igor@igordurbek.com>
2025-11-14 16:02:40 +0100

    otokoislandia to archive

--- __archive__/otokoislandia/src/main.cpp
+++ __archive__/otokoislandia/src/main.cpp
@@ -1,14 +1,12 @@
 #include <iostream>
 #include <toml++/toml.hpp>
 
-int graphics();
-
 int main() {
   std::cout << "<START>" << std::endl;
 
   try {
     const toml::table tbl = toml::parse_file(
-        R"(C:\igoroffline\nevenadaw\__practice__\otokoislandia\config\main.toml)");
+        R"(C:\igoroffline\nevenadaw\__archive__\otokoislandia\config\main.toml)");
     std::cout << tbl << std::endl;
   } catch (const toml::parse_error& err) {
     std::cerr << "Parsing failed:" << std::endl << err << std::endl;
