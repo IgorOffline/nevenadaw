0c4c535e166e08aef7cd548d495667f197f49081
Igor Durbek <igor@igordurbek.com>
2025-11-14 23:07:02 +0100

    +hopeternal glfw

---
+++ __practice__/hopeternal/src/main.cpp
@@ -0,0 +1,49 @@
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <toml++/toml.hpp>
+
+int glfw_graphics();
+
+int main() {
+  std::cout << "<START>" << std::endl;
+
+  try {
+    const toml::table tbl = toml::parse_file(
+        R"(C:\igoroffline\nevenadaw\__archive__\otokoislandia\config\main.toml)");
+    std::cout << tbl << std::endl;
+  } catch (const toml::parse_error& err) {
+    std::cerr << "Parsing failed:" << std::endl << err << std::endl;
+
+    return EXIT_FAILURE;
+  }
+
+  int graphics = glfw_graphics();
+
+  std::cout << "graphics: " << graphics << " <END>" << std::endl;
+
+  return EXIT_SUCCESS;
+}
+
+int glfw_graphics() {
+  if (!glfwInit()) return EXIT_FAILURE;
+
+  GLFWwindow* window = glfwCreateWindow(1280, 720, "hopeternal", NULL, NULL);
+
+  if (!window) {
+    glfwTerminate();
+    return EXIT_FAILURE;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  while (!glfwWindowShouldClose(window)) {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+
+  return EXIT_SUCCESS;
+}
