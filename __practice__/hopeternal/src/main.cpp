5933fca0edb8a1502c673cf284ee22a23a9f7466
IgorOffline <73474119+IgorOffline@users.noreply.github.com>
2025-11-15 01:08:07 +0100

    PR50 Hopeternal init
    PR50 Hopeternal init

---
+++ __practice__/hopeternal/src/main.cpp
@@ -0,0 +1,53 @@
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <toml++/toml.hpp>
+
+int glfw_graphics();
+
+int main() {
+  std::cout << "<START>" << std::endl;
+
+  try {
+    const toml::table tbl = toml::parse_file(
+        R"(C:\igoroffline\nevenadaw\__practice__\hopeternal\config\main.toml)");
+    std::cout << tbl << std::endl;
+  } catch (const toml::parse_error& err) {
+    std::cerr << "Parsing failed:" << std::endl << err << std::endl;
+    return EXIT_FAILURE;
+  }
+
+  const int graphics = glfw_graphics();
+
+  std::cout << "graphics: " << graphics << " <END>" << std::endl;
+
+  return EXIT_SUCCESS;
+}
+
+int glfw_graphics() {
+  if (!glfwInit()) return EXIT_FAILURE;
+
+  glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+  GLFWwindow* window =
+      glfwCreateWindow(1280, 720, "hopeternal 0.1.0", NULL, NULL);
+
+  if (!window) {
+    glfwTerminate();
+    return EXIT_FAILURE;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  glClearColor(0.1294f, 0.1294f, 0.1294f, 1.0f);
+
+  while (!glfwWindowShouldClose(window)) {
+    glClear(GL_COLOR_BUFFER_BIT);
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+
+  return EXIT_SUCCESS;
+}
