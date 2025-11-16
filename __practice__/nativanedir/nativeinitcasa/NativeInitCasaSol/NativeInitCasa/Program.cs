90a751e50078153c68b49371fa2010221d6ef5ee
Igor Durbek <igor@igordurbek.com>
2025-11-16 11:51:53 +0100

    chupavagalava INIT rectangle

---
+++ __practice__/nativanedir/nativeinitcasa/NativeInitCasaSol/NativeInitCasa/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace NativeInitCasa;
+
+internal static class Program
+{
+    private static void Main()
+    {
+        Console.WriteLine("<NativeInitCasa::START>");
+        Console.WriteLine("<NativeInitCasa::END>");
+    }
+}
