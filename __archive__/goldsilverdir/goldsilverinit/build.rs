b0e040f57d25fb6fe25893def932484f901cfeb4
Igor Durbek <igor@igordurbek.com>
2025-11-20 13:05:04 +0100

    Fix: ERR 1181

--- __archive__/goldsilverdir/goldsilverinit/build.rs
+++ __archive__/goldsilverdir/goldsilverinit/build.rs
@@ -1,4 +1,15 @@
+use std::path::PathBuf;
+
 fn main() {
+    let lib_path = PathBuf::from("./libs")
+        .canonicalize()
+        .expect("Could not canonicalize library path");
+
+    println!(
+        "cargo:rustc-link-search=native={}",
+        lib_path.to_str().unwrap()
+    );
+
     println!("cargo:rustc-link-lib=dylib=SDL2");
     println!("cargo:rustc-link-lib=dylib=SDL2_ttf");
 }
