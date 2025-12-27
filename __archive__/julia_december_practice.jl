a46a8ac874f05c804097abad5115bb90a1f5077f
IgorOffline <73474119+IgorOffline@users.noreply.github.com>
2025-12-27 01:34:13 +0100

    +julia_december_practice.jl

---
+++ __archive__/julia_december_practice.jl
@@ -0,0 +1,23 @@
+#
+# PART 1
+#
+# println("Hello, World!");
+
+#
+# PART 2
+#
+
+igoroffline_pi = π
+igoroffline_e = ℯ
+println("$igoroffline_pi ; $igoroffline_e")
+igoroffline_result_pi = 1000 + igoroffline_pi
+igoroffline_result_e = 1000 + igoroffline_e
+println("$igoroffline_result_pi ; $igoroffline_result_e")
+
+#
+# PART 3
+#
+
+#
+# PART 4
+#
