67e5d0e628d2f7cacd1e2d2d1242f98d88f4e454
IgorOffline <73474119+IgorOffline@users.noreply.github.com>
2026-01-04 10:00:03 +0100

    HaskellAnimalCatDog

--- __archive__/twentyhaskellhelloworld.hs
+++ __archive__/twentyhaskellhelloworld.hs
@@ -1,12 +1,31 @@
--- igoroffline :: ghc -o hask hask.hs
-data Animal = Animal 
-  { animalId  :: Int
-  , animalAge :: Int 
-  } deriving (Show)
+-- ghc -o hask hask.hs
+{-# LANGUAGE FlexibleInstances #-}
+
+class Animal a where
+    makeSound :: a -> String
+
+data Cat = Cat { catName :: String } deriving Show
+
+data Dog = Dog { dogName :: String } deriving Show
+
+instance Animal Cat where
+    makeSound _ = "Meow!"
+
+instance Animal Dog where
+    makeSound _ = "Woof!"
+
+makeEveryoneTalk :: (Functor f, Animal a) => f a -> f String
+makeEveryoneTalk container = fmap makeSound container
 
 main :: IO ()
 main = do
-    let firstAnimal = Animal { animalId = 1, animalAge = 18 }
-    let secondAnimal = Animal { animalId = 2, animalAge = 19 }
-    print firstAnimal
-    print secondAnimal
+    let cattyCat = Cat { catName = "Catty Cat" }
+    let doggyDog = Dog { dogName = "Doggy Dog" }
+    putStrLn $ (catName cattyCat) ++ " [cat things] " ++ (makeSound cattyCat)
+    putStrLn $ (dogName doggyDog) ++ " [dog things] " ++ (makeSound doggyDog)
+    let catList = [Cat "Luna", Cat "Milo"]
+    let maybeDog = Just (Dog "Buddy")
+    let noAnimal = Nothing :: Maybe Cat
+    print $ makeEveryoneTalk catList
+    print $ makeEveryoneTalk maybeDog
+    print $ makeEveryoneTalk noAnimal
