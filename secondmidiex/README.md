ef098450210b0b278552de76372d3791154973f3
IgorOffline <73474119+IgorOffline@users.noreply.github.com>
2026-01-12 10:05:37 +0100

    PR102 Elixir/Vital bridge part 2

--- secondmidiex/README.md
+++ secondmidiex/README.md
@@ -1,1 +1,19 @@
 # Secondmidiex
+
+Relevant commands:
+
+> aseqdump -p 24:0
+> 
+> aconnect -l
+> 
+> lsmod | grep virmidi
+> 
+> aconnect MIDIex:0 24:0
+> 
+> aseqdump -p MIDIex
+> 
+> aconnect -x
+> 
+> aconnect MIDIex:0 24:0
+>
+> aconnect MIDIex:0 14:0
--- secondmidiex/lib/secondmidiex.ex
+++ secondmidiex/lib/secondmidiex.ex
@@ -2,37 +2,36 @@ defmodule SecondMidiex do
   use Application
 
   def start(_type, _args) do
-    IO.puts("Starting SecondMidiex application...")
+    IO.puts("SecondMidiex")
     Task.start(fn -> play_midi() end)
     {:ok, self()}
   end
 
   def play_midi do
     conn = Midiex.create_virtual_output("ElixirOut")
-    
-    IO.puts("Created virtual port: #{conn.name}")
-    
-    Process.sleep(200)
-    auto_connect()
 
-    forever_loop(conn)
-  end
-
-  defp auto_connect do
-    case System.cmd("aconnect", ["ElixirOut", "VirMIDI 2-0"]) do
-      {_, 0} -> IO.puts("ALSA Patch Successful: ElixirOut -> VirMIDI 2-0")
-      {_, _} -> IO.puts("Patch failed. Try: aconnect ElixirOut 'VirMIDI 2-0'")
-    end
-  end
-
-  defp forever_loop(conn) do
-    IO.puts("Sending C4 to Vital...")
-    Midiex.send_msg(conn, <<0x90, 60, 100>>)
     Process.sleep(500)
-    
-    Midiex.send_msg(conn, <<0x80, 60, 0>>)
-    Process.sleep(1500)
-    
-    forever_loop(conn)
+
+    ["24:0", "14:0"] |> Enum.each(fn port ->
+      System.cmd("aconnect", ["MIDIex:0", port])
+    end)
+
+    sequence_loop(conn)
+  end
+
+  defp sequence_loop(conn) do
+    [60]
+    |> Enum.each(fn note ->
+      play_note(conn, note, 40, 200)
+    end)
+
+    Process.sleep(400)
+    sequence_loop(conn)
+  end
+
+  defp play_note(conn, note, vel, duration) do
+    Midiex.send_msg(conn, <<0x90, note, vel>>)
+    Process.sleep(duration)
+    Midiex.send_msg(conn, <<0x80, note, 0>>)
   end
 end
