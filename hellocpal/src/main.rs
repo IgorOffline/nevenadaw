79ccaf0eb2d0a0c7da53462187b4c3ee3d437804
Igor Durbek <igor@igordurbek.com>
2026-01-22 22:12:26 +0100

    +ASIO pt. 2

--- hellocpal/src/main.rs
+++ hellocpal/src/main.rs
@@ -1,17 +1,57 @@
 use cpal::traits::{DeviceTrait, HostTrait};
 
-#[link(name = "advapi32")]
-unsafe extern "C" {}
-
 fn main() {
     println!("<START>");
-    let host = cpal::default_host();
-    let output_device = host
-        .default_output_device()
-        .expect("failed to get default output device");
-    let id = output_device
-        .id()
-        .expect("failed to get default output device ID");
-    println!("Output device: {}", id);
+
+    let asio_host = cpal::host_from_id(cpal::HostId::Asio).ok();
+
+    if let Some(host) = asio_host {
+        println!("ASIO host found.");
+        let devices = host.output_devices().expect("failed to get output devices");
+        println!("Available ASIO output devices:");
+        for (i, device) in devices.enumerate() {
+            println!(
+                "  {}. {}",
+                i,
+                device
+                    .description()
+                    .expect("bfe931cc failed to get output device")
+                    .name()
+            );
+        }
+
+        match host.default_output_device() {
+            Some(device) => {
+                println!(
+                    "Default ASIO output device: {}",
+                    device
+                        .description()
+                        .expect("fdefd3bc failed to get output device")
+                        .name()
+                );
+            }
+            None => {
+                println!("No default ASIO output device found.");
+            }
+        }
+    } else {
+        println!("ASIO host not found.");
+    }
+
+    let default_host = cpal::default_host();
+    println!("Default host: {:?}", default_host.id());
+
+    if let Some(device) = default_host.default_output_device() {
+        println!(
+            "Default output device on default host: {}",
+            device
+                .description()
+                .expect("d68c786e failed to get output device")
+                .name()
+        );
+    } else {
+        println!("No default output device found on default host.");
+    }
+
     println!("<END>");
 }
