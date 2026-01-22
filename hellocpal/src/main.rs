42e1bed99f113a91bb260190b3f56772fcd98098
Igor Durbek <igor@igordurbek.com>
2026-01-22 22:02:24 +0100

    +wasapi

--- hellocpal/src/main.rs
+++ hellocpal/src/main.rs
@@ -1,3 +1,17 @@
+use cpal::traits::{DeviceTrait, HostTrait};
+
+#[link(name = "advapi32")]
+unsafe extern "C" {}
+
 fn main() {
-    println!("Hello, world!");
+    println!("<START>");
+    let host = cpal::default_host();
+    let output_device = host
+        .default_output_device()
+        .expect("failed to get default output device");
+    let id = output_device
+        .id()
+        .expect("failed to get default output device ID");
+    println!("Output device: {}", id);
+    println!("<END>");
 }
