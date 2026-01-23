7e715c9a11fa657b3d1501a0072906ae8d8214f0
Igor Durbek <igor@igordurbek.com>
2026-01-23 20:28:10 +0100

    +audio factory prep, unsafe part

--- audiomolekuladir/audiomolekula/audiomolekula_audio/src/lib.rs
+++ audiomolekuladir/audiomolekula/audiomolekula_audio/src/lib.rs
@@ -1,3 +1,37 @@
-pub fn add(a: i32, b: i32) -> i32 {
-    a + b
+use clap_sys::entry::clap_plugin_entry;
+use clap_sys::factory::plugin_factory::{clap_plugin_factory, CLAP_PLUGIN_FACTORY_ID};
+use libloading::{Library, Symbol};
+use std::ffi::CString;
+
+pub fn setup_audio_system() {
+    let plugin_path = r"C:\Program Files\Common Files\CLAP\Vital.clap";
+    let plugin_name = "Vital";
+
+    if !std::path::Path::new(plugin_path).exists() {
+        println!("{} not found at {}", plugin_name, plugin_path);
+        return;
+    }
+
+    let lib = unsafe {
+        Library::new(plugin_path).unwrap_or_else(|_| panic!("Failed to load {}", plugin_name))
+    };
+
+    let entry_symbol: Symbol<*const clap_plugin_entry> =
+        unsafe { lib.get(b"clap_entry\0").expect("Failed to get clap_entry") };
+    let entry = unsafe { &**entry_symbol };
+
+    let plugin_path_cstring = CString::new(plugin_path).unwrap();
+    unsafe { (entry.init.expect("Plugin init missing"))(plugin_path_cstring.as_ptr()) };
+
+    let factory_ptr = unsafe {
+        entry.get_factory.expect("get_factory missing")(CLAP_PLUGIN_FACTORY_ID.as_ptr() as *const i8)
+    };
+    let factory = unsafe { (factory_ptr as *const clap_plugin_factory).as_ref() };
+    if factory.is_none() {
+        println!("Plugin factory is null for {}", plugin_name);
+        return;
+    }
+    //let _factory = factory.unwrap();
+
+    println!("(Factory ready)");
 }
--- audiomolekuladir/audiomolekula/audiomolekula_core/src/lib.rs
+++ audiomolekuladir/audiomolekula/audiomolekula_core/src/lib.rs
@@ -1,9 +1,10 @@
 pub fn core() {
-    println!("<CORE::START> {}", audiomolekula_audio::add(2, 3));
+    println!("<CORE::START>");
     let coin_toss = rand::random_range(1..=2);
+    println!("coin_toss={}", coin_toss);
+    audiomolekula_audio::setup_audio_system();
     let regina = audiomolekula_io::audiomolekula_load_toml()
         .expect("aafcf099 Failed to load audiomolekula config");
-    println!("coin_toss= {}", coin_toss);
     println!("{}", regina);
     println!("<CORE::END>");
 }
