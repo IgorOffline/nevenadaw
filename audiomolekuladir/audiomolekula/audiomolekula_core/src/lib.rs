22861f6e171045713f361a4ea4041c0d04126e0a
Igor Durbek <igor@igordurbek.com>
2026-01-23 22:32:17 +0100

    +frontend_show_window

--- audiomolekuladir/audiomolekula/audiomolekula_core/src/lib.rs
+++ audiomolekuladir/audiomolekula/audiomolekula_core/src/lib.rs
@@ -3,9 +3,9 @@ pub fn core() {
     let coin_toss = rand::random_range(1..=2);
     println!("coin_toss={}", coin_toss);
     audiomolekula_audio::setup_audio_system();
-    audiomolekula_frontend::frontend_echo();
     let regina = audiomolekula_io::audiomolekula_load_toml()
         .expect("aafcf099 Failed to load audiomolekula config");
     println!("{}", regina);
+    audiomolekula_frontend::frontend_show_window();
     println!("<CORE::END>");
 }
