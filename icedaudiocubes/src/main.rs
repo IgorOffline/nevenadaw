4ec3eaa9dcb8e65cf0df1c7640b61fad611c4be9
Igor Durbek <igor@igordurbek.com>
2026-02-07 16:10:26 +0100

    INIT audiocrysis

--- icedaudiocubes/src/main.rs
+++ icedaudiocubes/src/main.rs
@@ -1,68 +1,60 @@
-use iced::{button, Alignment, Button, Column, Element, Sandbox, Settings, Text};
+use iced::canvas::{self, Canvas, Geometry, Program};
+use iced::{Color, Element, Rectangle, Sandbox, Settings};
 use icedaudiocubes::Audiocubes;
 use nih_plug::prelude::*;
 
 fn main() -> iced::Result {
     let uncoined_toss = rand::random_range(1000..=20_000);
-    if uncoined_toss > 19_999 {
+    if uncoined_toss < 19_999 {
         println!("Statistics is for the utmost unfortunate");
         nih_export_standalone::<Audiocubes>();
     } else {
         println!("<START>");
-        return Counter::run(Settings::default());
+        return HelloWorld::run(Settings::default());
     }
 
     Ok(())
 }
 
-#[derive(Default)]
-struct Counter {
-    value: i32,
-    increment_button: button::State,
-    decrement_button: button::State,
-}
+struct HelloWorld;
 
-#[derive(Debug, Clone, Copy)]
-enum Message {
-    IncrementPressed,
-    DecrementPressed,
-}
-
-impl Sandbox for Counter {
-    type Message = Message;
+impl Sandbox for HelloWorld {
+    type Message = ();
 
     fn new() -> Self {
-        Self::default()
+        Self
     }
 
     fn title(&self) -> String {
-        String::from("Counter - Iced")
+        String::from("Hello World Canvas - Iced")
     }
 
-    fn update(&mut self, message: Message) {
-        match message {
-            Message::IncrementPressed => {
-                self.value += 1;
-            }
-            Message::DecrementPressed => {
-                self.value -= 1;
-            }
-        }
-    }
+    fn update(&mut self, _message: Self::Message) {}
 
-    fn view(&mut self) -> Element<'_, Message> {
-        Column::new()
-            .padding(20)
-            .align_items(Alignment::Center)
-            .push(
-                Button::new(&mut self.increment_button, Text::new("Increment"))
-                    .on_press(Message::IncrementPressed),
-            )
-            .push(Text::new(self.value.to_string()).size(50))
-            .push(
-                Button::new(&mut self.decrement_button, Text::new("Decrement"))
-                    .on_press(Message::DecrementPressed),
-            )
+    fn view(&mut self) -> Element<'_, Self::Message> {
+        Canvas::new(HelloCanvas)
+            .width(iced::Length::Fill)
+            .height(iced::Length::Fill)
             .into()
+    }
+}
+
+struct HelloCanvas;
+
+impl Program<()> for HelloCanvas {
+    fn draw(&self, bounds: Rectangle, _cursor: canvas::Cursor) -> Vec<Geometry> {
+        let mut frame = canvas::Frame::new(bounds.size());
+
+        frame.fill_text(canvas::Text {
+            content: String::from("Hello, World!"),
+            position: bounds.center(),
+            color: Color::BLACK,
+            size: 40.0.into(),
+            horizontal_alignment: iced::alignment::Horizontal::Center,
+            vertical_alignment: iced::alignment::Vertical::Center,
+            ..Default::default()
+        });
+
+        vec![frame.into_geometry()]
     }
 }
