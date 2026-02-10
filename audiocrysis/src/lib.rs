1e84cd1f9062a55fda8a43d58b375bb6082e1015
IgorOffline <73474119+IgorOffline@users.noreply.github.com>
2026-02-10 07:00:47 +0100

    PR156 +(audiocrysis, icedaudiocubes)

---
+++ audiocrysis/src/lib.rs
@@ -0,0 +1,170 @@
+use nih_plug::prelude::*;
+use nih_plug_iced::*;
+use std::sync::Arc;
+
+pub struct Audiocrysis {
+    params: Arc<AudiocrysisParams>,
+}
+
+#[derive(Params)]
+pub struct AudiocrysisParams {
+    #[id = "gain"]
+    pub gain: FloatParam,
+}
+
+impl Default for Audiocrysis {
+    fn default() -> Self {
+        Self {
+            params: Arc::new(AudiocrysisParams::default()),
+        }
+    }
+}
+
+impl Default for AudiocrysisParams {
+    fn default() -> Self {
+        Self {
+            gain: FloatParam::new(
+                "Gain",
+                1.0,
+                FloatRange::Skewed {
+                    min: 0.0,
+                    max: 2.0,
+                    factor: FloatRange::gain_skew_factor(0.0, 2.0),
+                },
+            )
+            .with_unit(" gainfloat"),
+        }
+    }
+}
+
+impl Plugin for Audiocrysis {
+    const NAME: &'static str = "Audiocrysis";
+    const VENDOR: &'static str = "Igor Offline";
+    const URL: &'static str = "https://igordurbek.com";
+    const EMAIL: &'static str = "igor@igordurbek.com";
+
+    const VERSION: &'static str = "0.1.0";
+
+    const AUDIO_IO_LAYOUTS: &'static [AudioIOLayout] = &[AudioIOLayout {
+        main_input_channels: NonZeroU32::new(2),
+        main_output_channels: NonZeroU32::new(2),
+        aux_input_ports: &[],
+        aux_output_ports: &[],
+        names: PortNames::const_default(),
+    }];
+
+    type SysExMessage = ();
+    type BackgroundTask = ();
+
+    fn params(&self) -> Arc<dyn Params> {
+        self.params.clone()
+    }
+
+    fn editor(&mut self, _async_executor: AsyncExecutor<Self>) -> Option<Box<dyn Editor>> {
+        create(self.params.clone(), default_state())
+    }
+
+    fn process(
+        &mut self,
+        buffer: &mut Buffer,
+        _aux: &mut AuxiliaryBuffers,
+        _context: &mut impl ProcessContext<Self>,
+    ) -> ProcessStatus {
+        for channel in buffer.as_slice() {
+            for sample in channel.iter_mut() {
+                *sample *= self.params.gain.value();
+            }
+        }
+
+        ProcessStatus::Normal
+    }
+}
+
+impl ClapPlugin for Audiocrysis {
+    const CLAP_ID: &'static str = "com.igordurbek.audiocrysis";
+    const CLAP_DESCRIPTION: Option<&'static str> = Some("Audiocrysis");
+    const CLAP_MANUAL_URL: Option<&'static str> = Some(Self::URL);
+    const CLAP_SUPPORT_URL: Option<&'static str> = Some(Self::URL);
+    const CLAP_FEATURES: &'static [ClapFeature] = &[ClapFeature::AudioEffect, ClapFeature::Stereo];
+}
+
+pub(crate) fn default_state() -> Arc<IcedState> {
+    IcedState::from_size(300, 200)
+}
+
+pub(crate) fn create(
+    params: Arc<AudiocrysisParams>,
+    editor_state: Arc<IcedState>,
+) -> Option<Box<dyn Editor>> {
+    create_iced_editor::<AudiocrysisEditor>(editor_state, params)
+}
+
+struct AudiocrysisEditor {
+    params: Arc<AudiocrysisParams>,
+    context: Arc<dyn GuiContext>,
+
+    audiocrysis_slider_state: widgets::param_slider::State,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+    ParamUpdate(widgets::ParamMessage),
+}
+
+impl IcedEditor for AudiocrysisEditor {
+    type Executor = executor::Default;
+    type Message = Message;
+    type InitializationFlags = Arc<AudiocrysisParams>;
+
+    fn new(
+        params: Self::InitializationFlags,
+        context: Arc<dyn GuiContext>,
+    ) -> (Self, Command<Self::Message>) {
+        let editor = AudiocrysisEditor {
+            params,
+            context,
+
+            audiocrysis_slider_state: Default::default(),
+        };
+
+        (editor, Command::none())
+    }
+
+    fn context(&self) -> &dyn GuiContext {
+        self.context.as_ref()
+    }
+
+    fn update(
+        &mut self,
+        _window: &mut WindowQueue,
+        message: Self::Message,
+    ) -> Command<Self::Message> {
+        match message {
+            Message::ParamUpdate(message) => self.handle_param_message(message),
+        }
+
+        Command::none()
+    }
+
+    fn view(&mut self) -> Element<'_, Self::Message> {
+        Column::new()
+            .align_items(Alignment::Center)
+            .padding(20)
+            .spacing(10)
+            .push(
+                Text::new("Audiocrysis")
+                    .size(24)
+                    .height(30.into())
+                    .width(Length::Fill)
+                    .horizontal_alignment(alignment::Horizontal::Center)
+                    .vertical_alignment(alignment::Vertical::Center),
+            )
+            .push(
+                widgets::ParamSlider::new(&mut self.audiocrysis_slider_state, &self.params.gain)
+                    .map(Message::ParamUpdate),
+            )
+            .into()
+    }
+}
+
+nih_export_clap!(Audiocrysis);
--- icedaudiocubes/src/main.rs
+++ icedaudiocubes/src/main.rs
@@ -1,6 +1,60 @@
+use iced::canvas::{self, Canvas, Geometry, Program};
+use iced::{Color, Element, Rectangle, Sandbox, Settings};
 use icedaudiocubes::Audiocubes;
 use nih_plug::prelude::*;
 
-fn main() {
-    nih_export_standalone::<Audiocubes>();
+fn main() -> iced::Result {
+    let uncoined_toss = rand::random_range(1000..=20_000);
+    if uncoined_toss < 19_999 {
+        println!("Statistics is for the utmost unfortunate");
+        nih_export_standalone::<Audiocubes>();
+    } else {
+        println!("<START>");
+        return HelloWorld::run(Settings::default());
+    }
+
+    Ok(())
+}
+
+struct HelloWorld;
+
+impl Sandbox for HelloWorld {
+    type Message = ();
+
+    fn new() -> Self {
+        Self
+    }
+
+    fn title(&self) -> String {
+        String::from("Hello World Canvas - Iced")
+    }
+
+    fn update(&mut self, _message: Self::Message) {}
+
+    fn view(&mut self) -> Element<'_, Self::Message> {
+        Canvas::new(HelloCanvas)
+            .width(iced::Length::Fill)
+            .height(iced::Length::Fill)
+            .into()
+    }
+}
+
+struct HelloCanvas;
+
+impl Program<()> for HelloCanvas {
+    fn draw(&self, bounds: Rectangle, _cursor: canvas::Cursor) -> Vec<Geometry> {
+        let mut frame = canvas::Frame::new(bounds.size());
+
+        frame.fill_text(canvas::Text {
+            content: String::from("Hello, World!"),
+            position: bounds.center(),
+            color: Color::BLACK,
+            size: 40.0.into(),
+            horizontal_alignment: iced::alignment::Horizontal::Center,
+            vertical_alignment: iced::alignment::Vertical::Center,
+            ..Default::default()
+        });
+
+        vec![frame.into_geometry()]
+    }
 }
