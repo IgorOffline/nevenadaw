7d8c3e8a0692e39827df9b295838ed578cdfed88
IgorOffline <73474119+IgorOffline@users.noreply.github.com>
2026-02-19 08:55:32 +0100

    PR188 Bosonoga LOGI 1 2 , LOGI 9 pseudo-undefined

--- moradotase/src/tests.rs
+++ moradotase/src/tests.rs
@@ -1,13 +1,13 @@
-use crate::bosonoga::ProgramParser;
+use crate::bosonoga::BosonogaParser;
 use pretty_assertions::assert_eq;
 
 #[test]
 fn test_multiple_stmts() {
     let input = r"
-        ADA  DI  2 3 5  DO  EKI  DO
-        ADA  DI  2 3 5 10  DO  EKI  DO
+        ADA  DI  2 3 5  DO   LOGI 1   EKI  DO
+        ADA  DI  2 3 5 10  DO   LOGI 2   EKI  DO   LOGI 9
     ";
-    let parser = ProgramParser::new();
-    let program = parser.parse(input).unwrap();
-    assert_eq!(program, 30);
+    let parser = BosonogaParser::new();
+    let bosonoga = parser.parse(input).unwrap();
+    assert_eq!(bosonoga, 30);
 }
