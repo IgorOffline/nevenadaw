3026d0402e4f04b4debb1f3ff90e1fbdf1c6407c
IgorOffline <73474119+IgorOffline@users.noreply.github.com>
2026-02-19 09:59:35 +0100

    PR189 BOSONOGA main

--- moradotase/src/tests.rs
+++ moradotase/src/tests.rs
@@ -4,10 +4,10 @@ use pretty_assertions::assert_eq;
 #[test]
 fn test_multiple_stmts() {
     let input = r"
-        ADA  DI  2 3 5  DO   LOGI 1   EKI  DO
-        ADA  DI  2 3 5 10  DO   LOGI 2   EKI  DO   LOGI 9
+        BOSONOGA main
+        DI 2 3 5 DO
     ";
     let parser = BosonogaParser::new();
     let bosonoga = parser.parse(input).unwrap();
-    assert_eq!(bosonoga, 30);
+    assert_eq!(bosonoga, 1010);
 }
