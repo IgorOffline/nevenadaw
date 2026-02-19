d47b0ea625d005526a15325bfa91c296d79148f5
Igor Durbek <igor@igordurbek.com>
2026-02-19 06:54:18 +0100

    Bosonoga (2): 30 == 30

--- moradotase/src/tests.rs
+++ moradotase/src/tests.rs
@@ -3,7 +3,11 @@ use pretty_assertions::assert_eq;
 
 #[test]
 fn test_multiple_stmts() {
+    let input = r"
+        ADA  DI  2 3 5  DO  EKI  DO
+        ADA  DI  2 3 5 10  DO  EKI  DO
+    ";
     let parser = ProgramParser::new();
-    let program = parser.parse("    ADA  DI  2 3 5  DO  EKI  DO  ").unwrap();
-    assert_eq!(program, 10);
+    let program = parser.parse(input).unwrap();
+    assert_eq!(program, 30);
 }
