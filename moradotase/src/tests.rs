a205e0ee075577e4e865ac76124f0a54d8a4f7fa
Igor Durbek <igor@igordurbek.com>
2026-02-19 08:53:37 +0100

    Bosonoga (5): +LOGI 1 2, LOGI 9 pseudo-undefined

--- moradotase/src/tests.rs
+++ moradotase/src/tests.rs
@@ -4,8 +4,8 @@ use pretty_assertions::assert_eq;
 #[test]
 fn test_multiple_stmts() {
     let input = r"
-        ADA  DI  2 3 5  DO  LOGI 1 EKI  DO
-        ADA  DI  2 3 5 10  DO  LOGI 2 EKI  DO
+        ADA  DI  2 3 5  DO   LOGI 1   EKI  DO
+        ADA  DI  2 3 5 10  DO   LOGI 2   EKI  DO   LOGI 9
     ";
     let parser = BosonogaParser::new();
     let bosonoga = parser.parse(input).unwrap();
