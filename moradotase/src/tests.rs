1f00786acc5d8de71dcf4bbe1691342146ac49c6
Igor Durbek <igor@igordurbek.com>
2026-02-20 05:07:23 +0100

    +Bosonoga SUB-DI SUB-DO

--- moradotase/src/tests.rs
+++ moradotase/src/tests.rs
@@ -2,11 +2,21 @@ use crate::bosonoga::BosonogaParser;
 use pretty_assertions::assert_eq;
 
 #[test]
-fn test_bosonoga() {
+fn test_bosonoga_basic_identifiers() {
     let input = r"
-        DI   LOGI   SUB 200   DO   DI
+        DI   LOGI   ADA 2000   SUB 1000   DO
     ";
     let parser = BosonogaParser::new();
     let bosonoga = parser.parse(input).unwrap();
-    assert_eq!(bosonoga, 11);
+    assert_eq!(bosonoga, 1111);
+}
+
+#[test]
+fn test_bosonoga_multiple_subtractions() {
+    let input = r"
+        DI   SUB-DI 5 5 SUB-DO   DI
+    ";
+    let parser = BosonogaParser::new();
+    let bosonoga = parser.parse(input).unwrap();
+    assert_eq!(bosonoga, 190);
 }
