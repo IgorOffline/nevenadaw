44c4d8ad956b6de550e3c11758eb4d904cc6e2aa
Igor Durbek <igor@igordurbek.com>
2026-02-20 04:42:41 +0100

    +Bosonoga, Redesigned: Ident list

--- moradotase/src/tests.rs
+++ moradotase/src/tests.rs
@@ -4,9 +4,9 @@ use pretty_assertions::assert_eq;
 #[test]
 fn test_bosonoga() {
     let input = r"
-        DI   DO   DI
+        DI   LOGI   SUB 200   DO   DI
     ";
     let parser = BosonogaParser::new();
     let bosonoga = parser.parse(input).unwrap();
-    assert_eq!(bosonoga, 210);
+    assert_eq!(bosonoga, 11);
 }
