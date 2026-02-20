2a68db1f7ff0a0d4d42d2a902f4bd8a643db294c
Igor Durbek <igor@igordurbek.com>
2026-02-20 12:32:46 +0100

    +BosonogaItem

--- moradotase/src/main.rs
+++ moradotase/src/main.rs
@@ -2,6 +2,12 @@ use lalrpop_util::lalrpop_mod;
 
 lalrpop_mod!(pub bosonoga);
 
+#[derive(Debug)]
+pub enum BosonogaItem {
+    Add(String, i32),
+    Tali,
+}
+
 #[cfg(test)]
 mod tests;
 
--- moradotase/src/tests.rs
+++ moradotase/src/tests.rs
@@ -2,47 +2,16 @@ use crate::bosonoga::BosonogaParser;
 use pretty_assertions::assert_eq;
 
 #[test]
-fn test_bosonoga_basic_identifiers() {
+fn test_bosonoga_tali() {
     let input = r"
-        DI   LOGI   ADA 2000   SUB 1000   DO
+        DI
+        TALI
+        DO
+        TALI
+        DI
+        TALI
     ";
     let parser = BosonogaParser::new();
     let bosonoga = parser.parse(input).unwrap();
-    assert_eq!(bosonoga, 1111);
-}
-
-#[test]
-fn test_bosonoga_multiple_subtractions() {
-    let input = r"
-        DI   ADA-DI 10 10 ADA-DO   SUB-DI 5 5 SUB-DO   DI
-    ";
-    let parser = BosonogaParser::new();
-    let bosonoga = parser.parse(input).unwrap();
-    assert_eq!(bosonoga, 210);
-}
-
-#[test]
-fn test_bosonoga_loop() {
-    let input = r"
-        DI
-        VALA-DI 5
-          ADA 10
-        VALA-DO
-        DI
-    ";
-    let parser = BosonogaParser::new();
-    let bosonoga = parser.parse(input).unwrap();
-    assert_eq!(bosonoga, 250);
-}
-
-#[test]
-fn test_bosonoga_types() {
-    let input = r"
-        DI
-        BATIP BUL   BATIP INAT
-        DI
-    ";
-    let parser = BosonogaParser::new();
-    let bosonoga = parser.parse(input).unwrap();
-    assert_eq!(bosonoga, 200);
+    assert_eq!(bosonoga, 201);
 }
