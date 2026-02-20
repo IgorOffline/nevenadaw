5ac5b54902c0f2bffabfbfa507cd35fbb7b1e9fe
IgorOffline <73474119+IgorOffline@users.noreply.github.com>
2026-02-20 05:36:32 +0100

    PR192 +Bosonoga VALA loop

--- moradotase/src/tests.rs
+++ moradotase/src/tests.rs
@@ -2,11 +2,35 @@ use crate::bosonoga::BosonogaParser;
 use pretty_assertions::assert_eq;
 
 #[test]
-fn test_bosonoga() {
+fn test_bosonoga_basic_identifiers() {
     let input = r"
-        DI   DO   DI
+        DI   LOGI   ADA 2000   SUB 1000   DO
+    ";
+    let parser = BosonogaParser::new();
+    let bosonoga = parser.parse(input).unwrap();
+    assert_eq!(bosonoga, 1111);
+}
+
+#[test]
+fn test_bosonoga_multiple_subtractions() {
+    let input = r"
+        DI   ADA-DI 10 10 ADA-DO   SUB-DI 5 5 SUB-DO   DI
     ";
     let parser = BosonogaParser::new();
     let bosonoga = parser.parse(input).unwrap();
     assert_eq!(bosonoga, 210);
+}
+
+#[test]
+fn test_bosonoga_loop() {
+    let input = r"
+        DI
+        VALA-DI 5
+          ADA 10
+        VALA-DO
+        DI
+    ";
+    let parser = BosonogaParser::new();
+    let bosonoga = parser.parse(input).unwrap();
+    assert_eq!(bosonoga, 250);
 }
