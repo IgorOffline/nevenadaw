225c91f9e4f93d0f18bf8a6c90a6010db00679da
Igor Durbek <igor@igordurbek.com>
2026-02-21 16:34:06 +0100

    +Bosonoga :: Prepare NeoRaekordio pt. 3

--- moradotase/src/ast.rs
+++ moradotase/src/ast.rs
@@ -2,4 +2,5 @@
 pub enum BosonogaItem {
     Funak(String),
     NeoRaekordio(String),
+    Tali(String),
 }
--- moradotase/src/tests.rs
+++ moradotase/src/tests.rs
@@ -4,20 +4,12 @@ use pretty_assertions::assert_eq;
 #[test]
 fn test_bosonoga() {
     println!("test_bosonoga");
-    let input = "FUNAK first";
-    let parser = BosonogaParser::new();
-    let bosonoga = parser.parse(input).unwrap();
-    assert_eq!(bosonoga, 0);
-}
-
-#[test]
-fn test_bosonoga_multiple() {
-    println!("test_bosonoga_multiple");
     let input = r"
-        FUNAK first
-        FUNAK first
-        NEO-RAEKORDIO second
-        FUNAK first
+        FUNAK todomakeuniquepertype
+        FUNAK todomakeuniquepertype
+        NEO-RAEKORDIO todomakeuniquepertype
+        NEO-RAEKORDIO todomakeuniquepertype
+        TALI todomakeuniquepertype
     ";
     let parser = BosonogaParser::new();
     let bosonoga = parser.parse(input).unwrap();
