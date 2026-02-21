0a5bd5c3024635d41005779e0a1cd05623536ddf
IgorOffline <73474119+IgorOffline@users.noreply.github.com>
2026-02-21 12:11:40 +0100

    PR205 +Bosonoga :: Multiple Funak(String)

--- moradotase/src/tests.rs
+++ moradotase/src/tests.rs
@@ -4,7 +4,20 @@ use pretty_assertions::assert_eq;
 #[test]
 fn test_bosonoga() {
     println!("test_bosonoga");
-    let input = "";
+    let input = "FUNAK first";
+    let parser = BosonogaParser::new();
+    let bosonoga = parser.parse(input).unwrap();
+    assert_eq!(bosonoga, 0);
+}
+
+#[test]
+fn test_bosonoga_multiple() {
+    println!("test_bosonoga_multiple");
+    let input = r"
+        FUNAK first
+        FUNAK first
+        FUNAK first
+    ";
     let parser = BosonogaParser::new();
     let bosonoga = parser.parse(input).unwrap();
     assert_eq!(bosonoga, 0);
