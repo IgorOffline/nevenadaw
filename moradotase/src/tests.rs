c736e07c2e28f280314415e8a00c7e6abcec80bc
Igor Durbek <igor@igordurbek.com>
2026-02-22 11:08:00 +0100

    Bosonoga: +BTreeSet instead of BTreeMap

--- moradotase/src/tests.rs
+++ moradotase/src/tests.rs
@@ -1,7 +1,7 @@
-use crate::ast::{new_variable_i32, BosonogaVariable};
+use crate::ast::BosonogaVariable;
 use crate::bosonoga::BosonogaBTreeParser;
 use pretty_assertions::assert_eq;
-use std::collections::BTreeMap;
+use std::collections::BTreeSet;
 
 #[test]
 fn test_bosonoga_init() {
@@ -15,11 +15,11 @@ fn test_bosonoga_init() {
     ";
     let parser = BosonogaBTreeParser::new();
     let btree = parser.parse(input).unwrap();
-    let first_var = new_variable_i32("first".to_string(), 10);
-    let second_var = new_variable_i32("second".to_string(), 20);
-    let mut expected_btree: BTreeMap<String, BosonogaVariable> = BTreeMap::new();
-    expected_btree.insert("first".to_string(), first_var);
-    expected_btree.insert("second".to_string(), second_var);
+    let first_var = BosonogaVariable::new_i32("first".to_string(), 10);
+    let second_var = BosonogaVariable::new_i32("second".to_string(), 20);
+    let mut expected_btree: BTreeSet<BosonogaVariable> = BTreeSet::new();
+    expected_btree.insert(first_var);
+    expected_btree.insert(second_var);
     assert_eq!(btree, expected_btree);
 }
 
@@ -37,10 +37,10 @@ fn test_bosonoga_multiset() {
     ";
     let parser = BosonogaBTreeParser::new();
     let btree = parser.parse(input).unwrap();
-    let first_var = new_variable_i32("first".to_string(), 30);
-    let second_var = new_variable_i32("second".to_string(), 40);
-    let mut expected_btree: BTreeMap<String, BosonogaVariable> = BTreeMap::new();
-    expected_btree.insert("first".to_string(), first_var);
-    expected_btree.insert("second".to_string(), second_var);
+    let first_var = BosonogaVariable::new_i32("first".to_string(), 30);
+    let second_var = BosonogaVariable::new_i32("second".to_string(), 40);
+    let mut expected_btree: BTreeSet<BosonogaVariable> = BTreeSet::new();
+    expected_btree.insert(first_var);
+    expected_btree.insert(second_var);
     assert_eq!(btree, expected_btree);
 }
