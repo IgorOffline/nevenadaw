e65c486a97b2a845acd80a10f02b7dc1550d6ce9
IgorOffline <73474119+IgorOffline@users.noreply.github.com>
2026-02-22 06:43:10 +0100

    PR207 Bosonoga: +println

--- moradotase/src/tests.rs
+++ moradotase/src/tests.rs
@@ -7,35 +7,21 @@ fn test_bosonoga() {
     println!("test_bosonoga");
     let input = r"
         FUNAK first
-        FUNAK first
-        BUL
         BUL
         TALI second
-        TALI second
-        INAT
         INAT
     ";
     let parser = BosonogaParser::new();
     let bosonoga = parser.parse(input).unwrap();
-    assert_eq!(bosonoga.len(), 8);
+    assert_eq!(bosonoga.len(), 4);
     assert_eq!(
         bosonoga[0],
         BosonogaElement::Command(BosonogaCommand::Funak("first".to_string()))
     );
+    assert_eq!(bosonoga[1], BosonogaElement::Type(BosonogaType::Bul));
     assert_eq!(
-        bosonoga[1],
-        BosonogaElement::Command(BosonogaCommand::Funak("first".to_string()))
-    );
-    assert_eq!(bosonoga[2], BosonogaElement::Type(BosonogaType::Bul));
-    assert_eq!(bosonoga[3], BosonogaElement::Type(BosonogaType::Bul));
-    assert_eq!(
-        bosonoga[4],
+        bosonoga[2],
         BosonogaElement::Command(BosonogaCommand::Tali("second".to_string()))
     );
-    assert_eq!(
-        bosonoga[5],
-        BosonogaElement::Command(BosonogaCommand::Tali("second".to_string()))
-    );
-    assert_eq!(bosonoga[6], BosonogaElement::Type(BosonogaType::Inat));
-    assert_eq!(bosonoga[7], BosonogaElement::Type(BosonogaType::Inat));
+    assert_eq!(bosonoga[3], BosonogaElement::Type(BosonogaType::Inat));
 }
