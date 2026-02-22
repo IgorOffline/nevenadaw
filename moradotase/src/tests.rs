b3a0ff793c950d102bd0ec6c316756bca70e6951
IgorOffline <73474119+IgorOffline@users.noreply.github.com>
2026-02-22 09:31:41 +0100

    PR209 Bosonoga: +BosonogaNumber

--- moradotase/src/tests.rs
+++ moradotase/src/tests.rs
@@ -1,38 +1,16 @@
-use crate::ast::{BosonogaCommand, BosonogaElement, BosonogaType};
-use crate::bosonoga::{BosonogaBTreeParser, BosonogaParser};
+use crate::bosonoga::BosonogaBTreeParser;
 use pretty_assertions::assert_eq;
 use std::collections::BTreeMap;
-
-#[test]
-fn test_bosonoga() {
-    println!("test_bosonoga");
-    let input = r"
-        FUNAK first
-        BUL
-        TALI second
-        INAT
-    ";
-    let parser = BosonogaParser::new();
-    let bosonoga = parser.parse(input).unwrap();
-    assert_eq!(bosonoga.len(), 4);
-    assert_eq!(
-        bosonoga[0],
-        BosonogaElement::Command(BosonogaCommand::Funak("first".to_string()))
-    );
-    assert_eq!(bosonoga[1], BosonogaElement::Type(BosonogaType::Bul));
-    assert_eq!(
-        bosonoga[2],
-        BosonogaElement::Command(BosonogaCommand::Tali("second".to_string()))
-    );
-    assert_eq!(bosonoga[3], BosonogaElement::Type(BosonogaType::Inat));
-}
 
 #[test]
 fn test_bosonoga_btree() {
     println!("test_bosonoga_btree");
     let input = r"
-        FUNAK first
-        TALI second
+        FUNAK first 10
+        FUNAK second 20
+        BUL
+        INAT
+        TALI
     ";
     let parser = BosonogaBTreeParser::new();
     let btree = parser.parse(input).unwrap();
