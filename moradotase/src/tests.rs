b18135bc7664c62b2b0f24d4584d00f590b76498
Igor Durbek <igor@igordurbek.com>
2026-02-22 10:23:18 +0100

    Bosonoga: +multiset_test

--- moradotase/src/tests.rs
+++ moradotase/src/tests.rs
@@ -3,8 +3,8 @@ use pretty_assertions::assert_eq;
 use std::collections::BTreeMap;
 
 #[test]
-fn test_bosonoga_btree() {
-    println!("test_bosonoga_btree");
+fn test_bosonoga_init() {
+    println!("test_bosonoga_init");
     let input = r"
         SET first 10
         SET second 20
@@ -17,5 +17,25 @@ fn test_bosonoga_btree() {
     let mut expected_btree = BTreeMap::new();
     expected_btree.insert("first".to_string(), 10);
     expected_btree.insert("second".to_string(), 20);
+    assert_eq!(btree, expected_btree);
+}
+
+#[test]
+fn test_bosonoga_multiset() {
+    println!("test_bosonoga_multiset");
+    let input = r"
+        SET first 10
+        SET second 20
+        SET first 30
+        SET second 40
+        BUL
+        INAT
+        TALI
+    ";
+    let parser = BosonogaBTreeParser::new();
+    let btree = parser.parse(input).unwrap();
+    let mut expected_btree = BTreeMap::new();
+    expected_btree.insert("first".to_string(), 30);
+    expected_btree.insert("second".to_string(), 40);
     assert_eq!(btree, expected_btree);
 }
