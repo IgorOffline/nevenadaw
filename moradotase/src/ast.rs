83cd3fd4c288037ab1fa5cff051a2c5552891fc6
Igor Durbek <igor@igordurbek.com>
2026-02-22 04:17:39 +0100

    Bosonoga: +Elements

--- moradotase/src/ast.rs
+++ moradotase/src/ast.rs
@@ -3,3 +3,15 @@ pub enum BosonogaCommand {
     Funak(String),
     Tali(String),
 }
+
+#[derive(Debug, PartialEq)]
+pub enum BosonogaType {
+    Bul,
+    Inat,
+}
+
+#[derive(Debug, PartialEq)]
+pub enum BosonogaElement {
+    Command(BosonogaCommand),
+    Type(BosonogaType),
+}
--- moradotase/src/tests.rs
+++ moradotase/src/tests.rs
@@ -1,22 +1,41 @@
+use crate::ast::{BosonogaCommand, BosonogaElement, BosonogaType};
 use crate::bosonoga::BosonogaParser;
 use pretty_assertions::assert_eq;
 
 #[test]
 fn test_bosonoga() {
-    use crate::ast::BosonogaCommand;
     println!("test_bosonoga");
     let input = r"
         FUNAK first
         FUNAK first
+        BUL
+        BUL
         TALI second
         TALI second
+        INAT
+        INAT
     ";
     let parser = BosonogaParser::new();
     let bosonoga = parser.parse(input).unwrap();
-    println!("Return value: {:?}", bosonoga);
-    assert_eq!(bosonoga.len(), 4);
-    assert_eq!(bosonoga[0], BosonogaCommand::Funak("first".to_string()));
-    assert_eq!(bosonoga[1], BosonogaCommand::Funak("first".to_string()));
-    assert_eq!(bosonoga[2], BosonogaCommand::Tali("second".to_string()));
-    assert_eq!(bosonoga[3], BosonogaCommand::Tali("second".to_string()));
+    assert_eq!(bosonoga.len(), 8);
+    assert_eq!(
+        bosonoga[0],
+        BosonogaElement::Command(BosonogaCommand::Funak("first".to_string()))
+    );
+    assert_eq!(
+        bosonoga[1],
+        BosonogaElement::Command(BosonogaCommand::Funak("first".to_string()))
+    );
+    assert_eq!(bosonoga[2], BosonogaElement::Type(BosonogaType::Bul));
+    assert_eq!(bosonoga[3], BosonogaElement::Type(BosonogaType::Bul));
+    assert_eq!(
+        bosonoga[4],
+        BosonogaElement::Command(BosonogaCommand::Tali("second".to_string()))
+    );
+    assert_eq!(
+        bosonoga[5],
+        BosonogaElement::Command(BosonogaCommand::Tali("second".to_string()))
+    );
+    assert_eq!(bosonoga[6], BosonogaElement::Type(BosonogaType::Inat));
+    assert_eq!(bosonoga[7], BosonogaElement::Type(BosonogaType::Inat));
 }
