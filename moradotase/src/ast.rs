e09cb9ac86c513d720b1c688aade72dc7ebbeac0
Igor Durbek <igor@igordurbek.com>
2026-02-22 15:45:38 +0100

    Bosonoga: Bevy DSL

--- moradotase/src/ast.rs
+++ moradotase/src/ast.rs
@@ -1,3 +1,10 @@
+use bevy::color::Srgba;
+use bevy::prelude::{
+    default, App, Camera2d, ClearColor, Commands, PluginGroup, Sprite, Startup, Transform, Vec2,
+    Window, WindowPlugin,
+};
+use bevy::window::WindowResolution;
+use bevy::DefaultPlugins;
 use std::cmp::Ordering;
 use std::hash::{Hash, Hasher};
 
@@ -11,6 +18,7 @@ pub enum BosonogaValue {
 pub enum BosonogaCommand {
     Set(BosonogaType, String, BosonogaValue),
     Tali,
+    Game(i32, i32),
 }
 
 #[derive(Debug, PartialEq, Clone, Eq, Hash, PartialOrd, Ord)]
@@ -74,4 +82,31 @@ impl Ord for BosonogaVariable {
     fn cmp(&self, other: &Self) -> Ordering {
         self.name.cmp(&other.name)
     }
+}
+
+pub fn game_launch(window_width: u32, window_height: u32) {
+    App::new()
+        .insert_resource(ClearColor(Srgba::hex("#607D8B").unwrap().into()))
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                resolution: WindowResolution::new(window_width, window_height),
+                title: "Bosonoga".to_string(),
+                ..default()
+            }),
+            ..default()
+        }))
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2d::default());
+    commands.spawn((
+        Sprite {
+            color: Srgba::hex("#512DA8").unwrap().into(),
+            custom_size: Some(Vec2::new(50.0, 50.0)),
+            ..default()
+        },
+        Transform::default(),
+    ));
 }
