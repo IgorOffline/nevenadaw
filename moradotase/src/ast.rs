6ec555d9aaf99f6a004064d280549501bc0143f5
Igor Durbek <igor@igordurbek.com>
2026-02-24 03:15:31 +0100

    Bosonoga: +BosonogaCommand::SpawnRectangle

--- moradotase/src/ast.rs
+++ moradotase/src/ast.rs
@@ -1,14 +1,16 @@
-use bevy::DefaultPlugins;
+use crate::bosonoga::BosonogaParser;
 use bevy::color::Srgba;
 use bevy::prelude::{
-    App, ButtonInput, Camera2d, ClearColor, Commands, KeyCode, PluginGroup, Query, Res, ResMut,
-    Resource, Sprite, Startup, Transform, Update, Vec2, Window, WindowPlugin, default,
+    default, App, ButtonInput, Camera2d, ClearColor, Commands, KeyCode, PluginGroup, Query, Res,
+    ResMut, Resource, Sprite, Startup, Transform, Update, Vec2, Window, WindowPlugin,
 };
 use bevy::window::WindowResolution;
-use bevy_egui::{EguiContexts, EguiPlugin, EguiPrimaryContextPass, egui};
+use bevy::DefaultPlugins;
+use bevy_egui::{egui, EguiContexts, EguiPlugin, EguiPrimaryContextPass};
 use rand::RngExt;
 use std::cmp::Ordering;
 use std::collections::BTreeSet;
+use std::fs;
 use std::hash::{Hash, Hasher};
 
 #[derive(Debug, PartialEq, Clone, Eq, Hash, PartialOrd, Ord)]
@@ -120,6 +122,7 @@ pub fn game_launch(
         .add_plugins(EguiPlugin::default())
         .add_systems(Startup, game_launch_setup)
         .add_systems(Update, spawn_rectangle_system)
+        .add_systems(Update, runtime_input_system)
         .add_systems(EguiPrimaryContextPass, ui_main_layout_system)
         .run();
 }
@@ -230,4 +233,85 @@ fn ui_main_layout_system(mut contexts: EguiContexts, variables: Res<BosonogaVari
                 });
             }
         });
+}
+
+fn runtime_input_system(
+    mut commands: Commands,
+    mut variables: ResMut<BosonogaVariables>,
+    keyboard_input: Res<ButtonInput<KeyCode>>,
+    window: Query<&Window>,
+) {
+    if !keyboard_input.just_pressed(KeyCode::KeyT) {
+        return;
+    }
+
+    let Ok(input) = fs::read_to_string("runtime-input.bosonoga") else {
+        eprintln!("Failed to read runtime-input.bosonoga");
+        return;
+    };
+
+    let parser = BosonogaParser::new();
+    let Ok(commands_vec) = parser.parse(&input) else {
+        eprintln!("Failed to parse runtime-input.bosonoga");
+        return;
+    };
+
+    for element in commands_vec {
+        match element {
+            BosonogaElement::Command(BosonogaCommand::Set(
+                BosonogaType::Inat,
+                name,
+                BosonogaValue::Inat(v),
+            )) => {
+                variables.0.replace(BosonogaVariable::new_i32(name, v));
+            }
+            BosonogaElement::Command(BosonogaCommand::Set(
+                BosonogaType::Bul,
+                name,
+                BosonogaValue::Bul(v),
+            )) => {
+                variables.0.replace(BosonogaVariable::new_bool(name, v));
+            }
+            BosonogaElement::Command(BosonogaCommand::Tali) => {
+                println!("TALI[{:#?}]", variables.0);
+            }
+            BosonogaElement::Command(BosonogaCommand::SpawnRectangle) => {
+                if let Ok(window) = window.single() {
+                    let width = window.width();
+                    let height = window.height();
+
+                    let mut rng = rand::rng();
+                    let x = rng.random_range(-(width / 2.0)..(width / 2.0));
+                    let y = rng.random_range(-(height / 2.0)..(height / 2.0));
+
+                    commands.spawn((
+                        Sprite {
+                            color: Srgba::hex(RECTANGLE_COLOR).unwrap().into(),
+                            custom_size: Some(RECTANGLE_SIZE),
+                            ..default()
+                        },
+                        Transform::from_xyz(x, y, 0.0),
+                    ));
+
+                    let rectangle_count_inner = BosonogaVariable::new_i32("rectangle_count", 0);
+                    let current_count = match variables.0.get(&rectangle_count_inner) {
+                        Some(var) => match var.value {
+                            BosonogaValue::Inat(i) => i,
+                            _ => 0,
+                        },
+                        None => 0,
+                    };
+
+                    variables.0.replace(BosonogaVariable::new_i32(
+                        "rectangle_count",
+                        current_count + 1,
+                    ));
+                }
+            }
+            BosonogaElement::Command(BosonogaCommand::Game(_, _, _, _)) => {
+                // Ignore Game command during runtime execution
+            }
+            _ => {}
+        }
+    }
 }
