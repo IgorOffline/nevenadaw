f756b4a6c390933f0eafcdfd33b48f3d3540b29e
IgorOffline <73474119+IgorOffline@users.noreply.github.com>
2026-03-16 03:26:21 +0100

    PR279 RustyMarchMain Fetch example API

--- godotrustymarch/rusty-march-game/march/scripts/RustyMarchMain.cs
+++ godotrustymarch/rusty-march-game/march/scripts/RustyMarchMain.cs
@@ -6,17 +6,15 @@ public partial class RustyMarchMain : Node2D
     public override void _Ready()
     {
         GD.Print("RustyMarchMain 0.1.0");
-        GD.Print(new RustyImage("+RustyImage"));
+        var fetch = new RustyWeb().Get();
+        var fetchString = fetch?.ToString() ?? "(-)";
+        GD.Print($"{new RustyImage("+RustyImage")} {fetchString}");
 
         var icon = GetNodeOrNull<Sprite2D>("Icon");
         if (icon != null)
-        {
             GD.Print($"Icon found at: {icon.Position}");
-        }
         else
-        {
             GD.PrintErr("Icon node not found!");
-        }
     }
 
     public override void _Process(double delta)
