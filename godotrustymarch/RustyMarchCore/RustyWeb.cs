a0c97d8ef4a21e533847924659250359958c082a
Igor Durbek <igor@igordurbek.com>
2026-03-16 03:10:35 +0100

    RustyMarchMain fetch example html

---
+++ godotrustymarch/RustyMarchCore/RustyWeb.cs
@@ -0,0 +1,42 @@
+﻿using Tomlyn;
+using Tomlyn.Model;
+
+namespace RustyMarchCore;
+
+public class RustyWeb
+{
+    private static readonly HttpClient Client = new();
+
+    public bool? Get()
+    {
+        try
+        {
+            const string configPath = @"C:\Users\igor\dev\nevenadawdir\nevenadaw\godotrustymarch.toml";
+            string? url = null;
+
+            if (File.Exists(configPath))
+            {
+                var content = File.ReadAllText(configPath);
+                var model = TomlSerializer.Deserialize<TomlTable>(content);
+                if (model != null && model.TryGetValue("rustymarch", out var sectionObj) &&
+                    sectionObj is TomlTable section)
+                    if (section.TryGetValue("url", out var urlObj) && urlObj is string urlValue)
+                        url = urlValue;
+            }
+            else
+            {
+                return null;
+            }
+
+            if (string.IsNullOrEmpty(url)) return null;
+
+            using var request = new HttpRequestMessage(HttpMethod.Get, url);
+            using var response = Client.Send(request);
+            return response.IsSuccessStatusCode;
+        }
+        catch
+        {
+            return false;
+        }
+    }
+}
--- godotrustymarch/rusty-march-game/march/scripts/RustyMarchMain.cs
+++ godotrustymarch/rusty-march-game/march/scripts/RustyMarchMain.cs
@@ -6,17 +6,13 @@ public partial class RustyMarchMain : Node2D
     public override void _Ready()
     {
         GD.Print("RustyMarchMain 0.1.0");
-        GD.Print(new RustyImage("+RustyImage"));
+        GD.Print($"{new RustyImage("+RustyImage")} {new RustyWeb().Get()}");
 
         var icon = GetNodeOrNull<Sprite2D>("Icon");
         if (icon != null)
-        {
             GD.Print($"Icon found at: {icon.Position}");
-        }
         else
-        {
             GD.PrintErr("Icon node not found!");
-        }
     }
 
     public override void _Process(double delta)
